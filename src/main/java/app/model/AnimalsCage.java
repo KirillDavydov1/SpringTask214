a70245f6804b2b1e4026141efe15347f373838ac
KirillDavydov1 <82818161+KirillDavydov1@users.noreply.github.com>
2023-10-22 14:09:14 +0300

    22.10.2023

--- src/main/java/app/model/AnimalsCage.java
+++ src/main/java/app/model/AnimalsCage.java
@@ -1,19 +1,34 @@
 package app.model;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Component;
 
 @Component
 public class AnimalsCage {
 
     @Autowired
+    @Qualifier("myDog")
     private Animal animal;
+
+    @Autowired
+    private Timer timer;
 
     public void whatAnimalSay() {
         System.out.println("Say:");
         System.out.println(animal.toString());
         System.out.println("At:");
-        System.out.println(new Timer().getTime());
+        System.out.println(timer.getTime());
         System.out.println("________________________");
+    }
+
+    /*    @Autowired
+        @Qualifier("dog")*/
+    public void setAnimal(Animal animal) {
+        this.animal = animal;
+    }
+
+    public Timer getTimer() {
+        return timer;
     }
 }
